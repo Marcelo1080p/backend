0dcf0e766ed4239b92cc03346c6afb612348d583
Marcelo1080p <juniooor.2015@gmail.com>
2023-02-12 23:20:44 -0300

    fazendo o primeiro crud

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const todosRoutes = require('./todos.routes')
+const app = express();
+
+app.use(express.json())
+app.use(todosRoutes);
+
+app.get('/health', ( req, res ) => {
+    return res.json("up");
+})
+
+
+
+app.listen(3000, () => {
+    console.log("servidor iniciado")
+})
---
+++ src/todos.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const allTodos = [{ nome: "aaa", status: false }];
+const todosRoutes = express.Router();
+
+// C
+todosRoutes.post("/todos", ( req, res ) => {
+    const { name } = req.body;
+    allTodos.push({ name, status: false });
+    return res.status(201).json(allTodos);
+});
+
+// R
+todosRoutes.get("/todos", ( req, res ) => {
+    return res.status(200).json(allTodos);
+});
+// U
+// D
+
+
+module.exports = todosRoutes;
